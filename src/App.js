1aefbd3888a068765e3489886762ddbb95cb1fc9
Imtiaz <imtiaz.rashid.89@gmail.com>
2022-01-17 19:40:19 -0500

    successfully fetched data from API and displayed an output for the user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [message, setMessage] = useState("");
+  const [author, setAuthor] = useState("");
+  
+  // fetch json data from the stoic quotes api
+  // https://stoicquotesapi.com/docs
+  useEffect(() => {
+    fetch(`https://stoicquotesapi.com/v1/api/quotes/random`)
+      .then(res => res.json())
+      .then((jsonRes) => {
+        console.log(jsonRes);
+        setMessage(jsonRes.body);
+        setAuthor(jsonRes.author)
+      })
+  }, []);
+  // populate fetched data on to the screen
+
   return (
     <div className="App">
       <h1>Daily Stoic Quotes</h1>
+      <p>{message}</p>
+      <p>{author}</p>
     </div>
   );
 }
