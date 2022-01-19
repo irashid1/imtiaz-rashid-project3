cc74d3120db4595945089f12079ca19c50bcf335
Imtiaz <imtiaz.rashid.89@gmail.com>
2022-01-18 19:56:06 -0500

    added withOrientation fucntion to return a new array with the orientation of the image.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-// import axios from 'axios';
+// modules
+import axios from 'axios';
 import { useState, useEffect } from 'react';
+// components
 import PageHeading from "./PageHeading.js";
+// styling
 import './App.css';
 
 function App() {
 
   const [message, setMessage] = useState("");
   const [author, setAuthor] = useState("");
+
+  const [allPhotos, setAllPhotos] = useState([]);
 
   const handleSubmit = (event) => {
     // event.preventDefault();
@@ -25,6 +30,57 @@ function App() {
       })
   }, []);
   // populate fetched data on to the screen
+
+
+
+  useEffect(() => {
+    axios({
+      url: "https://api.unsplash.com/search/photos",
+      method: "GET",
+      dataResponse: "json",
+      params: {
+        query: "space",
+        per_page: 10,
+        client_id: "TKIetucrCKUutruuIA61j3V6l3Zxra12cwMPYIEuJ_4",
+      },
+    }).then((response) => {
+      // console.log(response);
+      const photos = response.data.results;
+      // const { results, total, total_pages } = response.data
+      // - this is returning the results array via obj deconstructing
+      // console.log(photos);
+      // console.log(results);
+
+      const withOrientation = photos.map((photo) => {
+        const ratio = photo.width / photo.height;
+        // const { width, height } = photo;
+
+        let orientation
+        // setting the orientation variable based on the ratio
+
+        if (ratio < 0.75) {
+          orientation = "portrait";
+        } else if (ratio > 1.35) {
+          orientation = "landscape";
+        } else {
+          orientation = "square";
+        }
+        // adding orientation property to our photo object
+
+        return { ...photo, orientation: orientation };
+      });
+      console.log(withOrientation);
+
+
+      // Updating state with our new array
+      setAllPhotos(withOrientation);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }, [])
+
+
+
 
   return (
     <div className="App">
