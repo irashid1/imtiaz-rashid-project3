306584cd9742f42d988df5f2d216d9d91109c068
Imtiaz <imtiaz.rashid.89@gmail.com>
2022-05-23 10:54:26 -0400

    changed the font for the button on the page

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 const Button = (props) => {
     return (
         // passing a function as a property triggered by event on click
-        <button type="submit" onClick={(event) => props.handlerFunction(event)}>{props.buttonText}</button>
+        <button className="quote-button" type="submit" onClick={(event) => props.handlerFunction(event)}>{props.buttonText}</button>
     )
 }
 
